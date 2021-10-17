b67a188ba996a75469d65a1d392b119cd8314065
Adrian Leung <adrian.leungssw@gmail.com>
2021-10-17 00:59:45 -0700

    start on search screen

--- pages/Search.jsx
+++ pages/Search.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { Searchbar } from 'react-native-paper';
+import {View, StyleSheet} from "react-native";
 
+const Search = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
 
-function Search() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  const onChangeSearch = query => setSearchQuery(query);
+
+  return (
+    <React.Fragment>
+      <Searchbar
+        placeholder="Trends, Tags, or users"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style = {styles.search}
+      />
+      <View>
         <Text>Search and destroy</Text>
       </View>
-    );
-  }
+    </React.Fragment>
+  );
+};
 
-  export {Search};
+// const SearchText = () => {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Search and destroy</Text>
+//     </View>
+//   );
+// }
+
+
+const styles = StyleSheet.create({
+  search:{
+    marginLeft:50,
+    marginRight:50,
+    borderRadius:40,
+    top:80,
+  },
+  searchText:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
+export {Search};
