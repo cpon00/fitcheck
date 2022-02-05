b3378398484af4c2b4d5b280ae0d0fe91ffed3fa
cpon00 <cpon712@gmail.com>
2022-02-04 16:57:04 -0800

    refactoring profile

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,5 @@
 import { getAuth } from '@firebase/auth'
-import {doc, getDoc} from '@firebase/firestore'
+import {doc, getDoc, orderBy, Timestamp} from '@firebase/firestore'
 import { USER_STATE_CHANGE } from '../constants/index';
 
 import {app, db} from '../../config'
@@ -13,6 +13,22 @@ export function fetchUser(){
     .then((snapshot) => {
       if(snapshot.exists){
         dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
+      } else {
+        console.log('DOES_NOT_EXIST')
+      }
+    })
+  })
+}
+
+export function fetchUserPosts(){
+  return ((dispatch) => {
+    const auth = getAuth()
+    const docRef = doc(db, 'posts', auth.currentUser.uid)
+    const docSnap = getDoc(docRef)
+    .then((snapshot) => {
+      if(snapshot.exists){
+        console.log(snapshot.docs)
+        console.log("DOCS HERE")
       } else {
         console.log('DOES_NOT_EXIST')
       }
--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -7,10 +7,18 @@ const initialState = {
 
 export const user = (state = initialState, action) => {
   switch (action.type) {
-    case USER_STATE_CHANGE
-  }
-  return {
-    ...state,
-    currentUser: action.currentUser
+    case USER_STATE_CHANGE:
+      return {
+        ...state,
+        currentUser: action.currentUser
+      }
+    case USER_POSTS_STATE_CHANGE:
+      return {
+        ...state,
+        posts: action.posts
+      }
+    default:
+      return state
+
   }
 }
